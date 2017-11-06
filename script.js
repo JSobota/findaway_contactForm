d5439478e3eb99aedf5b850ef066e85d5d2202ec
Justin Sobota <jusitn.sobota0227@gmail.com>
2017-11-06 00:38:28 -0500

    mobile responsive

---
+++ script.js
@@ -0,0 +1,7 @@
+var name = document.getElementById('name').value;
+var email=document.getElementById('email').value;
+var message=document.getElementById('message').value;
+
+documnet.getElementById('sendBtn').onclick{
+	alert(name);
+};
