1876125c751534b06e77f421237269a93108757b
Justin Sobota <jusitn.sobota0227@gmail.com>
2017-11-06 22:18:07 -0500

    cleaned up indentation

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function outputValue(){
-	var name;
-	var email;
-	var message;
+function outputValue() {
+    var name;
+    var email;
+    var message;
 
-	name=document.getElementById('name').value;
-	email=document.getElementById('email').value;
-	message=document.getElementById('message').value;
-	alert("Name: " + name + "   Email: " + email + "   Message: " + message);
+    name = document.getElementById('name').value;
+    email = document.getElementById('email').value;
+    message = document.getElementById('message').value;
+    alert("Name: " + name + "   Email: " + email + "   Message: " + message);
 
 }
